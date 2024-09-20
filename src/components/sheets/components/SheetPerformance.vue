ba31c783a654a3b32440c70b26aaf61be351bca3
Andrew Berry <andrew.berry@hearst.com>
2024-09-20 16:57:43 -0400

    Basic sheets stats table formed

--- src/components/sheets/components/SheetPerformance.vue
+++ src/components/sheets/components/SheetPerformance.vue
@@ -1,6 +1,57 @@
 <template>
   <div>
-    {{ sortedSheets }}
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th
+            scope="col"
+            v-for="prop in sortedSheets"
+            :key="prop.name"
+          >
+            {{ prop.name }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Hit</th>
+          <td
+            v-for="prop in sortedSheets"
+            :key="prop.name + ' Hit'"
+          >
+            {{ prop.hit }}
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Total</th>
+          <td
+            v-for="prop in sortedSheets"
+            :key="prop.name + ' Total'"
+          >
+            {{ prop.total }}
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Hit Rate</th>
+          <td
+            v-for="prop in sortedSheets"
+            :key="prop.name + ' Hit Rate'"
+          >
+            {{ (prop.hit * 100) / prop.total + '%' }}
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Perfect Weeks</th>
+          <td
+            v-for="prop in sortedSheets"
+            :key="prop.name + ' Perfect'"
+          >
+            {{ prop.perfect }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
@@ -34,3 +85,19 @@ export default {
   },
 }
 </script>
+<style scoped>
+table {
+  text-align: center;
+  border-collapse: collapse;
+}
+th,
+td {
+  padding: 0 5px;
+}
+table tr {
+  border-bottom: 1px solid var(--light-gray);
+}
+tbody th {
+  text-align: right;
+}
+</style>
